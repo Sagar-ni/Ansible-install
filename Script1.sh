4d0cedf087c5189f49d093acd7ef2c453c1a0f9a
Jackson Moris <161938805+Sagar-ni@users.noreply.github.com>
2024-03-04 16:38:51 +0530

    Add files via upload

---
+++ Script1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#This is my 1st shell script to print output
+echo "Hello All, Welcome to AWS/Devops Class"
+echo "Hello, How are you buddy?"
+ echo "We are learning Git"
+echo "We are learning about Git "
+#this is feature1
---
+++ Script2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#This script is to read input from the user/console
+echo "Value of a
+hello guys
+echo "Value of b"
+
