de8e4ed451d0d5e6e98645cf9361f93a1434e098
Jackson Moris <161938805+Sagar-ni@users.noreply.github.com>
2024-03-13 18:06:15 +0530

    Update Ansibleinstallation.sh

--- Ansibleinstallation.sh
+++ Ansibleinstallation.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#This is my 1st shell script to print output
-echo "Hello All, Welcome to AWS/Devops Class"
-echo "Hello, How are you buddy?"
- echo "We are learning Git"
-echo "We are learning about Git "
-#this is feature1 in new location..
+sudo apt install python3-pip
+ sudo apt update
+sudo apt install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt install ansible
